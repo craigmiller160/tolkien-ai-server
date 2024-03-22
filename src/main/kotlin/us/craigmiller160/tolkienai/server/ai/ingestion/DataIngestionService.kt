8ae47ef542796d0023833a8438128ad258457909
Craig Miller <craigmiller160@gmail.com>
2024-03-22 16:06:15 -0400

    Working on data ingestion

---
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/ai/ingestion/DataIngestionService.kt
@@ -0,0 +1,17 @@
+package us.craigmiller160.tolkienai.server.ai.ingestion
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Service
+import us.craigmiller160.tolkienai.server.config.RawSourcesProperties
+
+@Service
+class DataIngestionService{
+    private val log = LoggerFactory.getLogger(javaClass)
+    @EventListener(ApplicationReadyEvent::class)
+    fun ingest() {
+        log.info("Ingesting data")
+    }
+
+}
