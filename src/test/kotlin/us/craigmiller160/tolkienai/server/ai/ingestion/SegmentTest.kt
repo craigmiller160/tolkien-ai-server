7fc6421f874ccb2f817683a19ef7142cbe88d9b3
Craig Miller <craigmiller160@gmail.com>
2024-03-22 17:03:51 -0400

    Working on tests

--- src/test/kotlin/us/craigmiller160/tolkienai/server/ai/ingestion/SegmentTest.kt
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/ai/ingestion/SegmentTest.kt
@@ -1,23 +1,35 @@
 package us.craigmiller160.tolkienai.server.ai.ingestion
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import us.craigmiller160.tolkienai.server.ai.ingestion.service.Segment
+import us.craigmiller160.tolkienai.server.ai.ingestion.service.TitleLine
+import us.craigmiller160.tolkienai.server.ai.ingestion.service.createOrUpdateSegment
 import java.util.stream.Stream
 
 class SegmentTest {
     companion object {
         @JvmStatic
         fun createOrUpdateSegmentArgs(): Stream<CreateOrUpdateSegmentArg> {
-            TODO()
+            val baseSegment = Segment("TITLE", "Body", null)
+
+            return Stream.of(
+                CreateOrUpdateSegmentArg(null, "HELLO", Segment("HELLO", "", TitleLine("HELLO")))
+            )
         }
     }
     @ParameterizedTest
     @MethodSource("createOrUpdateSegmentArgs")
-    fun `creates or updates segment correctly`() {
-        TODO()
+    fun `creates or updates segment correctly`(arg: CreateOrUpdateSegmentArg) {
+        val actualSegment = createOrUpdateSegment(arg.previousSegment, arg.currentLine)
+        assertThat(actualSegment)
+            .isEqualTo(arg.expectedSegment)
     }
 
     data class CreateOrUpdateSegmentArg(
-        val line: String
+        val previousSegment: Segment?,
+        val currentLine: String,
+        val expectedSegment: Segment
     )
 }
