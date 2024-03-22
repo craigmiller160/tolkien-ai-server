715c84afc6dffbc270d7fc8227a711c14565f627
Craig Miller <craigmiller160@gmail.com>
2024-03-22 13:54:04 -0400

    Setting up trust store

--- src/main/kotlin/us/craigmiller160/tolkienai/server/TolkienAiApplication.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/TolkienAiApplication.kt
@@ -3,11 +3,21 @@ package us.craigmiller160.tolkienai.server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import java.io.File
 
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = ["us.craigmiller160.tolkienai.server"])
 class TolkienAiApplication
 
 fun main(args: Array<String>) {
+    setupTruststore()
     runApplication<TolkienAiApplication>(*args)
+}
+
+private fun setupTruststore() {
+    val truststorePath = TolkienAiApplication::class.java.classLoader.getResource("truststore.p12")!!
+        .toURI()
+        .let { File(it) }
+        .absolutePath
+    System.setProperty("javax.net.ssl.trustStore", truststorePath)
 }
