0278895c035455356e54a3af98325e3135982fea
Craig Miller <craigmiller160@gmail.com>
2024-03-22 14:46:54 -0400

    More parsing tweaks and more logging

--- src/main/kotlin/us/craigmiller160/tolkienai/server/ai/text/RawSourceParsing.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/ai/text/RawSourceParsing.kt
@@ -1,5 +1,6 @@
 package us.craigmiller160.tolkienai.server.ai.text
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
@@ -17,8 +18,12 @@ class RawSourceParsing(
     companion object {
         private const val TEMP_FILE = "temp-parsed.txt"
     }
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
     @EventListener(ApplicationReadyEvent::class)
     fun parse() {
+        log.info("Parsing raw Silmarillion text")
         Paths.get(System.getProperty("user.dir"), TEMP_FILE).bufferedWriter().use { writer ->
             File(rawSourcesProperties.silmarillion).bufferedReader().use { reader ->
                 reader.lines()
@@ -26,11 +31,13 @@ class RawSourceParsing(
                     .scan<String, LineWrapper?>(null) { previousLineWrapper, currentLine ->
                         lineToLineWrapper(previousLineWrapper?.line, currentLine)
                     }
+                    .filterNotNull()
                     .filter { it !is DeleteLine }
                     .forEach { lineWrapper ->
                         writer.write(lineWrapper.toText())
                     }
             }
         }
+        log.info("Raw Silmarillion text is parsed")
     }
 }
