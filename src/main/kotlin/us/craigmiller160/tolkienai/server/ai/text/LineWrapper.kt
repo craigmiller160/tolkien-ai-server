5fe6b6872b8d2782d432848454badeee69b3c04f
Craig Miller <craigmiller160@gmail.com>
2024-03-22 14:17:46 -0400

    Progress

---
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/ai/text/LineWrapper.kt
@@ -0,0 +1,15 @@
+package us.craigmiller160.tolkienai.server.ai.text
+
+sealed interface LineWrapper {
+    val line: String
+}
+
+data class ParagraphLine(override val line: String): LineWrapper
+
+data class TitleLine(override val line: String): LineWrapper
+
+data class DeleteLine(override val line: String): LineWrapper
+
+fun lineToLineWrapper(line: String): LineWrapper {
+
+}
---
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/ai/text/RawSourceParsing.kt
@@ -0,0 +1,28 @@
+package us.craigmiller160.tolkienai.server.ai.text
+
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import us.craigmiller160.tolkienai.server.config.RawSourcesProperties
+import java.io.File
+import java.nio.file.Paths
+import kotlin.io.path.bufferedReader
+import kotlin.io.path.bufferedWriter
+
+@Component
+class RawSourceParsing(
+    private val rawSourcesProperties: RawSourcesProperties
+) {
+    companion object {
+        private const val TEMP_FILE = "temp-parsed.txt"
+    }
+    @EventListener(ApplicationReadyEvent::class)
+    fun parse() {
+        Paths.get(System.getProperty("user.dir"), TEMP_FILE).bufferedWriter().use { writer ->
+            File(rawSourcesProperties.silmarillion).bufferedReader().use { reader ->
+                reader.lines()
+                    .map { it.trim() }
+            }
+        }
+    }
+}
