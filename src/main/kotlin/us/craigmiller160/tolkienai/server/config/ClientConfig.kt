4972b1368a5c32fdea470a85c909de9c4f88decf
Craig Miller <craigmiller160@gmail.com>
2024-03-22 13:30:10 -0400

    More config setup

--- src/main/kotlin/us/craigmiller160/tolkienai/server/config/ClientConfig.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/ClientConfig.kt
@@ -1,16 +1,33 @@
 package us.craigmiller160.tolkienai.server.config
 
+import com.aallam.openai.api.http.Timeout
+import com.aallam.openai.client.OpenAI
 import io.weaviate.client.Config
 import io.weaviate.client.WeaviateAuthClient
 import io.weaviate.client.WeaviateClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import kotlin.time.Duration.Companion.seconds
 
 @Configuration
 class ClientConfig {
+
     @Bean
-    fun weaviateClient(props: WeaviateProperties): WeaviateClient {
-        val config = Config(props.scheme, props.host)
-        return WeaviateAuthClient.apiKey(config, props.key)
-    }
+    fun weaviateClient(props: WeaviateProperties): WeaviateClient =
+        Config(props.scheme, props.host)
+            .let { WeaviateAuthClient.apiKey(config, props.key) }
+
+    @Bean
+    fun openaiClient(props: OpenaiProperties): OpenAI =
+        OpenAI(
+            token = props.key,
+            timeout = props.timeouts.toTimeout()
+        )
 }
+
+private fun OpenaiTimeoutProperties.toTimeout(): Timeout =
+    Timeout(
+        socket = this.socketSeconds.seconds,
+        connect = this.connectSeconds.seconds,
+        request = this.requestSeconds.seconds
+    )
--- src/main/kotlin/us/craigmiller160/tolkienai/server/config/OpenaiProperties.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/OpenaiProperties.kt
@@ -2,7 +2,14 @@ package us.craigmiller160.tolkienai.server.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
+data class OpenaiTimeoutProperties(
+    val requestSeconds: Int,
+    val socketSeconds: Int,
+    val connectSeconds: Int
+)
+
 @ConfigurationProperties(prefix = "tolkienai.openai")
 data class OpenaiProperties(
-    val key: String
+    val key: String,
+    val timeouts: OpenaiTimeoutProperties
 )
