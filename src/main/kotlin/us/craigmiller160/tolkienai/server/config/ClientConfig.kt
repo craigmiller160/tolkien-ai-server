008462e72ffda2e49b8df1630402c820768a7022
Craig Miller <craigmiller160@gmail.com>
2024-03-22 13:24:42 -0400

    More setup

--- src/main/kotlin/us/craigmiller160/tolkienai/server/config/ClientConfig.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/ClientConfig.kt
@@ -1,7 +1,16 @@
 package us.craigmiller160.tolkienai.server.config
 
+import io.weaviate.client.Config
+import io.weaviate.client.WeaviateAuthClient
+import io.weaviate.client.WeaviateClient
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ClientConfig {
+    @Bean
+    fun weaviateClient(props: WeaviateProperties): WeaviateClient {
+        val config = Config(props.scheme, props.host)
+        return WeaviateAuthClient.apiKey(config, props.key)
+    }
 }
