98d035ead78bb6f5ec98f33419038210035c6fb9
Craig Miller <craigmiller160@gmail.com>
2024-03-25 12:33:47 -0400

    progress

--- src/main/kotlin/us/craigmiller160/tolkienai/server/config/WebSecurityConfig.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/WebSecurityConfig.kt
@@ -3,6 +3,7 @@ package us.craigmiller160.tolkienai.server.config
 import io.craigmiller160.springkeycloakoauth2resourceserver.security.KeycloakOAuth2ResourceServerProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -10,6 +11,7 @@ import org.springframework.security.web.DefaultSecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class WebSecurityConfig(
     private val keycloakOAuth2ResourceServerProvider: KeycloakOAuth2ResourceServerProvider
 ) {
