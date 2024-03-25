049b3d34af8517971985ef666b961e7ca3814fca
Craig Miller <craigmiller160@gmail.com>
2024-03-24 21:23:30 -0400

    Redesigned tests

--- src/test/kotlin/us/craigmiller160/tolkienai/server/ai/ingestion/SegmentTest.kt
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/ai/ingestion/SegmentTest.kt
@@ -15,69 +15,38 @@ class SegmentTest {
   companion object {
     @JvmStatic
     fun createOrUpdateSegmentArgs(): Stream<CreateOrUpdateSegmentArg> {
-      // If the line is a title, and the previous line is a title, append to the previous title
-      // If the line is a NewLine or a DeleteLine, it's an error
-      // If the line is a title, and the previous line is not, start a new Segment with the title
-      // If the line is a paragraph, and there is no previous segment, it's an error
-      // If the line is a paragraph, and the previous segment has no title, it's an error
-      // If the line is a paragraph, and the previous segment has a title and content, start a new
-      // Segment with the previous title and new content
-      // If the line is a paragraph, and the previous segment has no content but has a title, append
-      // to the previous Segment
-      // If the line is a paragraph, and the previous segment has content & a title, but the content
-      // doesn't have an "ending", append to previous content
 
       return Stream.of(
           CreateOrUpdateSegmentArg(null, "HELLO", Result.success(Segment("HELLO", ""))),
           CreateOrUpdateSegmentArg(
-              Segment("HELLO", ""), "WORLD", Result.success(Segment("HELLO WORLD", ""))))
-
-      //      return Stream.of(
-      //          CreateOrUpdateSegmentArg(
-      //              null, "HELLO", Result.success(Segment("HELLO", "", TitleLine("HELLO")))),
-      //          CreateOrUpdateSegmentArg(
-      //              baseSegment, "HELLO", Result.success(Segment("HELLO", "",
-      // TitleLine("HELLO")))),
-      //          CreateOrUpdateSegmentArg(
-      //              baseSegment.copy(content = "", previousLineWrapper = TitleLine("TITLE")),
-      //              "HELLO",
-      //              Result.success(Segment("TITLE HELLO", "", TitleLine("HELLO")))),
-      //          CreateOrUpdateSegmentArg(
-      //              baseSegment,
-      //              "World",
-      //              Result.success(
-      //                  baseSegment.copy(
-      //                      content = "World", previousLineWrapper = ParagraphLine("World")))),
-      //          CreateOrUpdateSegmentArg(
-      //              null,
-      //              "World",
-      //              Result.failure(
-      //                  InvalidSegmentException(
-      //                      "No previous segment with a title to append content to. Line:
-      // World"))),
-      //          CreateOrUpdateSegmentArg(
-      //              baseSegment.copy(title = ""),
-      //              "World",
-      //              Result.failure(
-      //                  InvalidSegmentException(
-      //                      "Previous segment has no title, cannot append content. Line:
-      // World"))),
-      //          CreateOrUpdateSegmentArg(
-      //              baseSegment,
-      //              "",
-      //              Result.failure(InvalidSegmentException("Invalid line wrapper: DeleteLine.
-      // Line: "))),
-      //          CreateOrUpdateSegmentArg(
-      //              baseSegment.copy(previousLineWrapper = TitleLine("TITLE")),
-      //              "",
-      //              Result.failure(InvalidSegmentException("Invalid line wrapper: NewLine. Line:
-      // "))),
-      //          CreateOrUpdateSegmentArg(
-      //              baseSegment.copy(previousLineWrapper = TitleLine("TITLE"), content = ""),
-      //              "World",
-      //              Result.success(
-      //                  baseSegment.copy(
-      //                      content = "World", previousLineWrapper = ParagraphLine("World")))))
+              Segment("HELLO", ""), "WORLD", Result.success(Segment("HELLO WORLD", ""))),
+          CreateOrUpdateSegmentArg(
+              Segment("HELLO", ""), "World", Result.success(Segment("HELLO", "World"))),
+          CreateOrUpdateSegmentArg(
+              Segment("HELLO", "World"),
+              "Universe",
+              Result.success(Segment("HELLO", "World Universe"))),
+          CreateOrUpdateSegmentArg(
+              Segment("HELLO", "World."), "UNIVERSE", Result.success(Segment("UNIVERSE", ""))),
+          CreateOrUpdateSegmentArg(
+              Segment("HELLO", "World."), "Universe", Result.success(Segment("HELLO", "Universe"))),
+          CreateOrUpdateSegmentArg(
+              Segment("HELLO", "World"),
+              "",
+              Result.failure(InvalidSegmentException("Invalid line wrapper: DeleteLine"))),
+          CreateOrUpdateSegmentArg(
+              null,
+              "Universe",
+              Result.failure(
+                  InvalidSegmentException(
+                      "No previous segment with a title to append content to. Line: Universe"))),
+          CreateOrUpdateSegmentArg(
+              Segment("", ""),
+              "Universe",
+              Result.failure(
+                  InvalidSegmentException(
+                      "Previous segment has no title, cannot append content. Line: Universe")),
+          ))
     }
 
     @JvmStatic
