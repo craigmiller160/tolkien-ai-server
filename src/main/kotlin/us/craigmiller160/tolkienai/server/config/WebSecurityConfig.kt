5bcc1fc794cc5b1251f86c69b43a808a5a81213f
Craig Miller <craigmiller160@gmail.com>
2024-03-24 22:41:53 -0400

    Integrated security

---
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/WebSecurityConfig.kt
@@ -0,0 +1,30 @@
+package us.craigmiller160.tolkienai.server.config
+
+import io.craigmiller160.springkeycloakoauth2resourceserver.security.KeycloakOAuth2ResourceServerProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.DefaultSecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig(
+    private val keycloakOAuth2ResourceServerProvider: KeycloakOAuth2ResourceServerProvider
+) {
+  @Bean
+  fun configure(http: HttpSecurity): DefaultSecurityFilterChain =
+      http
+          .csrf { it.disable() }
+          .oauth2ResourceServer(keycloakOAuth2ResourceServerProvider.provideWebMvc())
+          .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+          .authorizeHttpRequests {
+            it.requestMatchers(
+                    "/actuator/health", "/v3/api-docs", "/v3/api-docs/*", "/swagger-ui/*")
+                .permitAll()
+                .requestMatchers("/**")
+                .hasAnyRole("access")
+          }
+          .build()
+}
