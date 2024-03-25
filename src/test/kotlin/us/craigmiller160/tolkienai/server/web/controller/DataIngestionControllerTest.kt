aeafc06dc7fd483bf46a789fa5527d0d90183f23
Craig Miller <craigmiller160@gmail.com>
2024-03-25 12:44:33 -0400

    Stubbed out test

---
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/web/controller/DataIngestionControllerTest.kt
@@ -0,0 +1,10 @@
+package us.craigmiller160.tolkienai.server.web.controller
+
+import org.junit.jupiter.api.Test
+
+class DataIngestionControllerTest {
+  @Test
+  fun `validate that admin access is required to ingest data`() {
+    TODO()
+  }
+}
