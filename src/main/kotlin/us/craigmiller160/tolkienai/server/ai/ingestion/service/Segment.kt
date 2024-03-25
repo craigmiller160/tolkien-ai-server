d5c2354cf98876bf404df04b11b6eab6d5f382f1
Craig Miller <craigmiller160@gmail.com>
2024-03-24 21:05:42 -0400

    Commented out old code

--- src/main/kotlin/us/craigmiller160/tolkienai/server/ai/ingestion/service/Segment.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/ai/ingestion/service/Segment.kt
@@ -1,7 +1,6 @@
 package us.craigmiller160.tolkienai.server.ai.ingestion.service
 
 import java.util.UUID
-import us.craigmiller160.tolkienai.server.ai.ingestion.exception.InvalidSegmentException
 
 data class Segment(
     val title: String,
@@ -13,44 +12,46 @@ data class Segment(
 }
 
 fun createOrUpdateSegment(previousSegment: Segment?, currentLine: String): Segment {
-  val lineWrapper = lineToLineWrapper(previousSegment?.previousLineWrapper?.line, currentLine)
-  if (lineWrapper is NewLine || lineWrapper is DeleteLine) {
-    throw InvalidSegmentException(
-        "Invalid line wrapper: ${lineWrapper.javaClass.simpleName}. Line: $currentLine")
-  }
-
-  if (lineWrapper is TitleLine && previousSegment?.previousLineWrapper is TitleLine) {
-    return previousSegment.copy(
-        title = "${previousSegment.title} ${lineWrapper.line}", previousLineWrapper = lineWrapper)
-  }
-
-  if (lineWrapper is TitleLine) {
-    return Segment(title = lineWrapper.line, content = "", previousLineWrapper = lineWrapper)
-  }
-
-  if (lineWrapper is ParagraphLine && previousSegment == null) {
-    throw InvalidSegmentException(
-        "No previous segment with a title to append content to. Line: $currentLine")
-  }
-
-  if (lineWrapper is ParagraphLine && previousSegment?.title.isNullOrBlank()) {
-    throw InvalidSegmentException(
-        "Previous segment has no title, cannot append content. Line: $currentLine")
-  }
-
-  if (lineWrapper is ParagraphLine &&
-      previousSegment != null &&
-      previousSegment.content.isNotBlank()) {
-    return Segment(
-        title = previousSegment.title,
-        content = lineWrapper.line,
-        previousLineWrapper = lineWrapper)
-  }
-
-  if (lineWrapper is ParagraphLine && previousSegment != null) {
-    return previousSegment.copy(content = currentLine, previousLineWrapper = lineWrapper)
-  }
-
-  throw InvalidSegmentException(
-      "Unknown set of conditions, unable to create or update segment. Line: $currentLine")
+  //  val lineWrapper = lineToLineWrapper(previousSegment?.previousLineWrapper?.line, currentLine)
+  //  if (lineWrapper is NewLine || lineWrapper is DeleteLine) {
+  //    throw InvalidSegmentException(
+  //        "Invalid line wrapper: ${lineWrapper.javaClass.simpleName}. Line: $currentLine")
+  //  }
+  //
+  //  if (lineWrapper is TitleLine && previousSegment?.previousLineWrapper is TitleLine) {
+  //    return previousSegment.copy(
+  //        title = "${previousSegment.title} ${lineWrapper.line}", previousLineWrapper =
+  // lineWrapper)
+  //  }
+  //
+  //  if (lineWrapper is TitleLine) {
+  //    return Segment(title = lineWrapper.line, content = "", previousLineWrapper = lineWrapper)
+  //  }
+  //
+  //  if (lineWrapper is ParagraphLine && previousSegment == null) {
+  //    throw InvalidSegmentException(
+  //        "No previous segment with a title to append content to. Line: $currentLine")
+  //  }
+  //
+  //  if (lineWrapper is ParagraphLine && previousSegment?.title.isNullOrBlank()) {
+  //    throw InvalidSegmentException(
+  //        "Previous segment has no title, cannot append content. Line: $currentLine")
+  //  }
+  //
+  //  if (lineWrapper is ParagraphLine &&
+  //      previousSegment != null &&
+  //      previousSegment.content.isNotBlank()) {
+  //    return Segment(
+  //        title = previousSegment.title,
+  //        content = lineWrapper.line,
+  //        previousLineWrapper = lineWrapper)
+  //  }
+  //
+  //  if (lineWrapper is ParagraphLine && previousSegment != null) {
+  //    return previousSegment.copy(content = currentLine, previousLineWrapper = lineWrapper)
+  //  }
+  //
+  //  throw InvalidSegmentException(
+  //      "Unknown set of conditions, unable to create or update segment. Line: $currentLine")
+  TODO()
 }
