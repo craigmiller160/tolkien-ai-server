08c12cbe31b0cff50f69116b56bbea61d61c021d
Craig Miller <craigmiller160@gmail.com>
2024-03-25 12:39:23 -0400

    Progress

--- src/main/kotlin/us/craigmiller160/tolkienai/server/web/controller/DataIngestionController.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/web/controller/DataIngestionController.kt
@@ -1,7 +1,6 @@
 package us.craigmiller160.tolkienai.server.web.controller
 
 import org.springframework.http.HttpStatus
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,7 +14,7 @@ import us.craigmiller160.tolkienai.server.web.type.IngestDataRequest
 class DataIngestionController(private val dataIngestionService: DataIngestionService) {
   @PostMapping
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  @PreAuthorize("hasAuthority('ADMIN')")
+  //  @PreAuthorize("hasAuthority('ADMIN')")
   fun ingestData(@RequestBody request: IngestDataRequest) {
     dataIngestionService.ingestSilmarillion(request.dryRun)
   }
