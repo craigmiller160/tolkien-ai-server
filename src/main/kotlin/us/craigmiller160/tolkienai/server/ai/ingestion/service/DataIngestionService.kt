de08b73ea0aaf4a515c12a46d3bfbcc5c9a4fe25
Craig Miller <craigmiller160@gmail.com>
2024-03-26 19:14:48 -0400

    Progress

--- src/main/kotlin/us/craigmiller160/tolkienai/server/ai/ingestion/service/DataIngestionService.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/ai/ingestion/service/DataIngestionService.kt
@@ -1,21 +1,35 @@
 package us.craigmiller160.tolkienai.server.ai.ingestion.service
 
-import com.aallam.openai.client.OpenAI
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
+import us.craigmiller160.tolkienai.server.ai.dto.EmbeddingContainer
 import us.craigmiller160.tolkienai.server.ai.ingestion.service.parsing.RawSourceParsingService
+import us.craigmiller160.tolkienai.server.ai.service.OpenAiService
 
 @Service
 class DataIngestionService(
     private val rawSourceParsingService: RawSourceParsingService,
     private val environment: Environment,
-    private val openAiClient: OpenAI
+    private val openAiService: OpenAiService
 ) {
   fun ingest(dryRun: Boolean) {
     if (dryRun && !dryRunAllowed()) {
       throw IllegalArgumentException("Dry runs are only allowed in the dev environment")
     }
-    val segments = rawSourceParsingService.parseSilmarillion(dryRun)
+
+    runBlocking {
+      rawSourceParsingService.parseSilmarillion(dryRun).let { createAllEmbeddings(it) }
+    }
+  }
+
+  private suspend fun createAllEmbeddings(
+      segments: List<String>
+  ): List<Deferred<EmbeddingContainer>> = coroutineScope {
+    segments.map { text -> async { openAiService.createEmbedding(text) } }
   }
 
   private fun dryRunAllowed(): Boolean = environment.matchesProfiles("dev")
