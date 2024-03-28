5ea93be6f12275c548c6a790a76c21d15ec90854
Craig Miller <craigmiller160@gmail.com>
2024-03-28 19:50:52 -0400

    Stubbed out test

---
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/web/controller/ChatControllerTest.kt
@@ -0,0 +1,10 @@
+package us.craigmiller160.tolkienai.server.web.controller
+
+import org.junit.jupiter.api.Test
+
+class ChatControllerTest {
+  @Test
+  fun `tests the controller`() {
+    TODO()
+  }
+}
