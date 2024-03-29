202350e427cc56f367a73f1380043db1681f50f3
Craig Miller <craigmiller160@gmail.com>
2024-03-29 18:08:33 -0400

    Updating mongo config

--- src/main/kotlin/us/craigmiller160/tolkienai/server/config/MongoConfig.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/MongoConfig.kt
@@ -1,5 +1,8 @@
 package us.craigmiller160.tolkienai.server.config
 
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.util.Date
 import java.util.UUID
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,7 +15,12 @@ import org.springframework.data.mongodb.core.convert.MongoCustomConversions
 class MongoConfig {
   @Bean
   fun mongoCustomConversions(): MongoCustomConversions =
-      listOf(UuidToStringConverter(), StringToUuidConverter()).let { MongoCustomConversions(it) }
+      listOf(
+              UuidToStringConverter(),
+              StringToUuidConverter(),
+              ZonedDateTimeToDateConverter(),
+              DateToZonedDateTimeConverter())
+          .let { MongoCustomConversions(it) }
 }
 
 @WritingConverter
@@ -23,4 +31,15 @@ class UuidToStringConverter : Converter<UUID, String> {
 @ReadingConverter
 class StringToUuidConverter : Converter<String, UUID> {
   override fun convert(source: String): UUID = UUID.fromString(source)
+}
+
+@WritingConverter
+class ZonedDateTimeToDateConverter : Converter<ZonedDateTime, Date> {
+  override fun convert(source: ZonedDateTime): Date =
+      source.withZoneSameInstant(ZoneId.of("UTC")).toInstant().let { Date.from(it) }
+}
+
+@ReadingConverter
+class DateToZonedDateTimeConverter : Converter<Date, ZonedDateTime> {
+  override fun convert(source: Date): ZonedDateTime = source.toInstant().atZone(ZoneId.of("UTC"))
 }
