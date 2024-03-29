a1d0d97aa3ebc3116338e511878ea6fe0fde0e08
Craig Miller <craigmiller160@gmail.com>
2024-03-28 20:05:56 -0400

    Integrated configurable query limit

--- src/main/kotlin/us/craigmiller160/tolkienai/server/web/service/ChatService.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/web/service/ChatService.kt
@@ -5,20 +5,23 @@ import org.springframework.stereotype.Service
 import us.craigmiller160.tolkienai.server.ai.dto.floatEmbedding
 import us.craigmiller160.tolkienai.server.ai.service.OpenAiService
 import us.craigmiller160.tolkienai.server.ai.service.WeaviateService
+import us.craigmiller160.tolkienai.server.config.ChatProperties
 import us.craigmiller160.tolkienai.server.web.type.ChatRequest
 import us.craigmiller160.tolkienai.server.web.type.ChatResponse
 
 @Service
 class ChatService(
     private val weaviateService: WeaviateService,
-    private val openAiService: OpenAiService
+    private val openAiService: OpenAiService,
+    private val chatProperties: ChatProperties
 ) {
 
   private val log = LoggerFactory.getLogger(javaClass)
   suspend fun chat(request: ChatRequest): ChatResponse {
     log.info("Preparing chat for query: ${request.query}")
     openAiService.createEmbedding(request.query).let { queryEmbedding ->
-      weaviateService.searchForEmbeddings(queryEmbedding.floatEmbedding, 10)
+      weaviateService.searchForEmbeddings(
+          queryEmbedding.floatEmbedding, chatProperties.query.recordLimit)
     }
     TODO()
   }
