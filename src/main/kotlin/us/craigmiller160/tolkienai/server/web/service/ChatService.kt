e2c248fc5854d2be6e9d07de57dbe13258164eb0
Craig Miller <craigmiller160@gmail.com>
2024-03-28 21:45:45 -0400

    Implemented the response

--- src/main/kotlin/us/craigmiller160/tolkienai/server/web/service/ChatService.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/web/service/ChatService.kt
@@ -1,5 +1,6 @@
 package us.craigmiller160.tolkienai.server.web.service
 
+import java.util.UUID
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -7,6 +8,7 @@ import us.craigmiller160.tolkienai.server.ai.dto.floatEmbedding
 import us.craigmiller160.tolkienai.server.ai.service.OpenAiService
 import us.craigmiller160.tolkienai.server.ai.service.WeaviateService
 import us.craigmiller160.tolkienai.server.config.ChatProperties
+import us.craigmiller160.tolkienai.server.web.type.ChatExplanation
 import us.craigmiller160.tolkienai.server.web.type.ChatRequest
 import us.craigmiller160.tolkienai.server.web.type.ChatResponse
 
@@ -19,13 +21,20 @@ class ChatService(
 
   private val log = LoggerFactory.getLogger(javaClass)
   fun chat(request: ChatRequest): ChatResponse {
-    log.info("Preparing chat for query: ${request.query}")
-    runBlocking {
-      openAiService.createEmbedding(request.query).let { queryEmbedding ->
-        weaviateService.searchForEmbeddings(
-            queryEmbedding.floatEmbedding, chatProperties.query.recordLimit)
-      }
+    val id = UUID.randomUUID()
+    log.info("Preparing chat. Chat ID: $id Query: ${request.query}")
+    return runBlocking {
+      val results =
+          openAiService.createEmbedding(request.query).let { queryEmbedding ->
+            weaviateService.searchForEmbeddings(
+                queryEmbedding.floatEmbedding, chatProperties.query.recordLimit)
+          }
+
+      ChatResponse(
+          chatId = id,
+          response = "",
+          explanation =
+              ChatExplanation(query = request.query, embeddingMatches = results.map { it.text }))
     }
-    TODO()
   }
 }
