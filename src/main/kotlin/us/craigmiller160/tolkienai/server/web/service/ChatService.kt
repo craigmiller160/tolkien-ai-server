4f0ab6a0d72f01c291d9537e77d7b9442ac4b78e
Craig Miller <craigmiller160@gmail.com>
2024-03-28 22:10:42 -0400

    working on chat integration

--- src/main/kotlin/us/craigmiller160/tolkienai/server/web/service/ChatService.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/web/service/ChatService.kt
@@ -4,6 +4,7 @@ import java.util.UUID
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import us.craigmiller160.tolkienai.server.ai.dto.ChatMessageRole
 import us.craigmiller160.tolkienai.server.ai.dto.floatEmbedding
 import us.craigmiller160.tolkienai.server.ai.service.OpenAiService
 import us.craigmiller160.tolkienai.server.ai.service.WeaviateService
@@ -24,11 +25,20 @@ class ChatService(
     val id = UUID.randomUUID()
     log.info("Preparing chat. Chat ID: $id Query: ${request.query}")
     return runBlocking {
-      val results =
-          openAiService.createEmbedding(request.query).let { queryEmbedding ->
-            weaviateService.searchForEmbeddings(
-                queryEmbedding.floatEmbedding, chatProperties.query.recordLimit)
-          }
+      val textMatches =
+          openAiService
+              .createEmbedding(request.query)
+              .let { queryEmbedding ->
+                weaviateService.searchForEmbeddings(
+                    queryEmbedding.floatEmbedding, chatProperties.query.recordLimit)
+              }
+              .joinToString("\n") { it.text }
+
+      openAiService.createChat(
+          ChatMessageRole.USER to request.query,
+          ChatMessageRole.SYSTEM to
+              "You are an expert on the works of JRR Tolkien and passionate about educating others on it. You will answer relying only on the provided list of data.",
+          ChatMessageRole.USER to "List of data: \n$textMatches")
 
       ChatResponse(
           chatId = id,
