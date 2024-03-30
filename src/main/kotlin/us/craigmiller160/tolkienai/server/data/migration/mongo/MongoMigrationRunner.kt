dafe8400ab2e889878f3fc677c4b153d25750ecf
Craig Miller <craigmiller160@gmail.com>
2024-03-30 09:33:22 -0400

    Working on migration running logic

--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigrationRunner.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigrationRunner.kt
@@ -3,6 +3,7 @@ package us.craigmiller160.tolkienai.server.data.migration.mongo
 import com.mongodb.client.MongoClient
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Component
+import us.craigmiller160.tolkienai.server.data.migration.Migration
 
 @Component
 class MongoMigrationRunner(
@@ -12,5 +13,9 @@ class MongoMigrationRunner(
   fun run() {
 
     TODO()
+  }
+
+  private fun <T> runMigration(index: Int, migration: Migration<T>, helper: T) {
+    migration.migrate(helper)
   }
 }
