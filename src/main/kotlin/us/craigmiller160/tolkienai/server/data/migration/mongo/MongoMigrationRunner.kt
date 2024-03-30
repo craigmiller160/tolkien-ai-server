95282fcea1327056dc8fb283a186c82a290ff7df
Craig Miller <craigmiller160@gmail.com>
2024-03-30 09:45:34 -0400

    Improving migration

--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigrationRunner.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigrationRunner.kt
@@ -7,8 +7,10 @@ import kotlin.io.path.readBytes
 import org.apache.commons.codec.binary.Hex
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.findAll
 import org.springframework.stereotype.Component
 import us.craigmiller160.tolkienai.server.data.migration.Migration
+import us.craigmiller160.tolkienai.server.data.migration.MigrationRecord
 import us.craigmiller160.tolkienai.server.data.migration.mongo.migrations.V001_InitialSchema
 
 @Component
@@ -17,9 +19,19 @@ class MongoMigrationRunner(
     private val mongoTemplate: MongoTemplate,
     @Value("\${spring.data.mongodb.database}") private val database: String
 ) {
+  companion object {
+    private const val MONGO_MIGRATION_HISTORY_COLLECTION = "mongo_migration_history"
+  }
+
+  private val db = client.getDatabase(database)
+
+  private val registeredMigrations: List<RegisteredMigration<*>> =
+      listOf(RegisteredMigration(index = 1, migration = V001_InitialSchema(), helper = db))
+
   fun run() {
-    val db = client.getDatabase(database)
-    runMigration(1, V001_InitialSchema(), db)
+    val records =
+        mongoTemplate.findAll(MigrationRecord::class.java, MONGO_MIGRATION_HISTORY_COLLECTION)
+    println(records)
   }
 
   private fun <T> runMigration(index: Int, migration: Migration<T>, helper: T) {
@@ -34,3 +46,5 @@ private fun generateHash(migration: Migration<T>): String {
   val digest = MessageDigest.getInstance("SHA-256")
   return Paths.get(uri).readBytes().let { digest.digest(it) }.let { Hex.encodeHexString(it) }
 }
+
+data class RegisteredMigration<T>(val index: Int, val migration: Migration<T>, val helper: T)
