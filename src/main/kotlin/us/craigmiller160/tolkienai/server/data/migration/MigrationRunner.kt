91046f8734d653b88b520348e36d5c537da6e820
Craig Miller <craigmiller160@gmail.com>
2024-03-30 09:35:32 -0400

    Updating runners

--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationRunner.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationRunner.kt
@@ -1,6 +1,13 @@
 package us.craigmiller160.tolkienai.server.data.migration
 
+import jakarta.annotation.PostConstruct
 import org.springframework.stereotype.Component
 import us.craigmiller160.tolkienai.server.data.migration.mongo.MongoMigrationRunner
 
-@Component class MigrationRunner(private val mongoMigrationRunner: MongoMigrationRunner) {}
+@Component
+class MigrationRunner(private val mongoMigrationRunner: MongoMigrationRunner) {
+  @PostConstruct
+  fun run() {
+    mongoMigrationRunner.run()
+  }
+}
--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigrationRunner.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigrationRunner.kt
@@ -1,21 +1,25 @@
 package us.craigmiller160.tolkienai.server.data.migration.mongo
 
 import com.mongodb.client.MongoClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Component
 import us.craigmiller160.tolkienai.server.data.migration.Migration
+import us.craigmiller160.tolkienai.server.data.migration.mongo.migrations.V001_InitialSchema
 
 @Component
 class MongoMigrationRunner(
     private val client: MongoClient,
-    private val mongoTemplate: MongoTemplate
+    private val mongoTemplate: MongoTemplate,
+    @Value("\${spring.data.mongodb.database}") private val database: String
 ) {
   fun run() {
-
-    TODO()
+    val db = client.getDatabase(database)
+    runMigration(1, V001_InitialSchema(), db)
   }
 
   private fun <T> runMigration(index: Int, migration: Migration<T>, helper: T) {
-    migration.migrate(helper)
+    val classLocation = migration.javaClass.protectionDomain.codeSource.location
+    //    migration.migrate(helper)
   }
 }
