6c9e4bd907b6c23807893110f8f549a83d187b8a
Craig Miller <craigmiller160@gmail.com>
2024-03-30 09:53:43 -0400

    Progress

--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigrationRunner.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigrationRunner.kt
@@ -5,9 +5,12 @@ import java.nio.file.Paths
 import java.security.MessageDigest
 import kotlin.io.path.readBytes
 import org.apache.commons.codec.binary.Hex
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.findAll
+import org.springframework.data.mongodb.core.find
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Component
 import us.craigmiller160.tolkienai.server.data.migration.Migration
 import us.craigmiller160.tolkienai.server.data.migration.MigrationRecord
@@ -23,19 +26,26 @@ class MongoMigrationRunner(
     private const val MONGO_MIGRATION_HISTORY_COLLECTION = "mongo_migration_history"
   }
 
+  private val log = LoggerFactory.getLogger(javaClass)
   private val db = client.getDatabase(database)
 
   private val registeredMigrations: List<RegisteredMigration<*>> =
-      listOf(RegisteredMigration(index = 1, migration = V001_InitialSchema(), helper = db))
+      listOf(RegisteredMigration(migration = V001_InitialSchema(), helper = db))
 
   fun run() {
-    val records =
-        mongoTemplate.findAll(MigrationRecord::class.java, MONGO_MIGRATION_HISTORY_COLLECTION)
-    println(records)
+    val historyRecords =
+        Query().with(Sort.by(Sort.Direction.ASC, "index")).let { query ->
+          mongoTemplate.find(query, MigrationRecord::class.java, MONGO_MIGRATION_HISTORY_COLLECTION)
+        }
+    registeredMigrations.forEachIndexed { index, registeredMigration ->
+      val historyRecord = historyRecords[index]
+      if (!historyRecord) {
+        registeredMigration.run()
+      }
+    }
   }
 
-  private fun <T> runMigration(index: Int, migration: Migration<T>, helper: T) {
-    val hash = generateHash(migration)
+  private fun <T> runMigration(migration: Migration<T>, helper: T) {
     //    migration.migrate(helper)
   }
 }
@@ -47,4 +57,6 @@ private fun generateHash(migration: Migration<*>): String {
   return Paths.get(uri).readBytes().let { digest.digest(it) }.let { Hex.encodeHexString(it) }
 }
 
-data class RegisteredMigration<T>(val index: Int, val migration: Migration<T>, val helper: T)
+data class RegisteredMigration<T>(val migration: Migration<T>, val helper: T)
+
+fun <T> RegisteredMigration<T>.run() = migration.migrate(helper)
