2b63bb37a65102cd12c77913d951cb10a53d064c
Craig Miller <craigmiller160@gmail.com>
2024-03-30 10:17:24 -0400

    Updated test

---
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigrationRunnerTest.kt
@@ -0,0 +1,16 @@
+package us.craigmiller160.tolkienai.server.data.migration.mongo
+
+import org.junit.jupiter.api.Test
+
+class MongoMigrationRunnerTest {
+  @Test
+  fun `performs migration`() {
+    /*
+     * 1) Successful new migrations
+     * 2) No new migrations to perform
+     * 3) Migration at index with invalid name
+     * 4) Migration at index with invalid hash
+     */
+    TODO()
+  }
+}
