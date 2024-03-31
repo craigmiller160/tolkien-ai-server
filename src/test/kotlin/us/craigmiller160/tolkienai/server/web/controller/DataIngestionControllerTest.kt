5848c494ffa6dcffdfca418157f0cdf8535c5842
Craig Miller <craigmiller160@gmail.com>
2024-03-31 11:20:56 -0400

    Disabled some tests

--- src/test/kotlin/us/craigmiller160/tolkienai/server/web/controller/DataIngestionControllerTest.kt
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/web/controller/DataIngestionControllerTest.kt
@@ -1,9 +1,11 @@
 package us.craigmiller160.tolkienai.server.web.controller
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class DataIngestionControllerTest {
   @Test
+  @Disabled
   fun `validate that admin access is required to ingest data`() {
     TODO()
   }
