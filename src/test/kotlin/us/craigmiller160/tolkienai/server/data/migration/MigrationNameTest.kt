86d0392030bfdb18c55861a344b48e8c6188e626
Craig Miller <craigmiller160@gmail.com>
2024-03-31 12:41:58 -0400

    Implemented tests

--- src/test/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationNameTest.kt
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationNameTest.kt
@@ -7,12 +7,24 @@ import java.util.stream.Stream
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import us.craigmiller160.tolkienai.server.data.migration.exception.MigrationException
+import us.craigmiller160.tolkienai.server.data.migration.other.BadMockMigration
+import us.craigmiller160.tolkienai.server.data.migration.test_migrations.V20240330__InitialMigration
 
 class MigrationNameTest {
   companion object {
     @JvmStatic
     fun migrationNameArgs(): Stream<MigrationNameArg> {
-      return Stream.of()
+      return Stream.of(
+          MigrationNameArg(
+              migration = V20240330__InitialMigration(),
+              expectedResult =
+                  Result.success(MigrationName(version = "20240330", name = "InitialMigration"))),
+          MigrationNameArg(
+              migration = BadMockMigration(),
+              expectedResult =
+                  Result.failure(
+                      MigrationException(
+                          "Migration at index 1 has invalid name: ${BadMockMigration::class.java.simpleName}"))))
     }
   }
 
