1e65937dc81784e1bad5060f5d5098e9e57b2739
Craig Miller <craigmiller160@gmail.com>
2024-03-31 11:13:53 -0400

    More mock migrations

---
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/data/migration/migrations/V1_20240331__MigrationTwo.kt
@@ -0,0 +1,5 @@
+package us.craigmiller160.tolkienai.server.data.migration.migrations
+
+import us.craigmiller160.tolkienai.server.data.migration.other.AbstractMockMigration
+
+class V1_20240331__MigrationTwo : AbstractMockMigration()
