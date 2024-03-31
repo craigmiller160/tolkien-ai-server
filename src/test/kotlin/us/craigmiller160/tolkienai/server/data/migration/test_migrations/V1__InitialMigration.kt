ff502751475bfe189895da1383d310688e092f52
Craig Miller <craigmiller160@gmail.com>
2024-03-31 11:19:14 -0400

    Progress

--- src/test/kotlin/us/craigmiller160/tolkienai/server/data/migration/test_migrations/V1__InitialMigration.kt
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/data/migration/test_migrations/V1__InitialMigration.kt
@@ -1,4 +1,4 @@
-package us.craigmiller160.tolkienai.server.data.migration.migrations
+package us.craigmiller160.tolkienai.server.data.migration.test_migrations
 
 import us.craigmiller160.tolkienai.server.data.migration.other.AbstractMockMigration
 
