8f576f2f233382ea71d02cdc11e01a4bc939e2a4
Craig Miller <craigmiller160@gmail.com>
2024-03-31 11:13:11 -0400

    Working on migrations

---
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/data/migration/migrations/V1__InitialMigration.kt
@@ -0,0 +1,5 @@
+package us.craigmiller160.tolkienai.server.data.migration.migrations
+
+import us.craigmiller160.tolkienai.server.data.migration.other.AbstractMockMigration
+
+class V1__InitialMigration : AbstractMockMigration()
