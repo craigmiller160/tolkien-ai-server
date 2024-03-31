2f1a1f4869af46eeb802c89d7d71eddc8a9537b8
Craig Miller <craigmiller160@gmail.com>
2024-03-31 14:37:42 -0400

    Refactoring existing migration helpers

--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigration.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/MongoMigration.kt
@@ -1,6 +1,5 @@
 package us.craigmiller160.tolkienai.server.data.migration.mongo
 
-import com.mongodb.client.MongoDatabase
 import us.craigmiller160.tolkienai.server.data.migration.Migration
 
-interface MongoCoreMigration : Migration<MongoDatabase>
+interface MongoMigration : Migration<MongoMigrationHelper>
--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/migrations/V001_InitialSchema.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/mongo/migrations/V001_InitialSchema.kt
@@ -1,13 +1,13 @@
 package us.craigmiller160.tolkienai.server.data.migration.mongo.migrations
 
-import com.mongodb.client.MongoDatabase
 import com.mongodb.client.model.Indexes
-import us.craigmiller160.tolkienai.server.data.migration.mongo.MongoCoreMigration
+import us.craigmiller160.tolkienai.server.data.migration.mongo.MongoMigration
+import us.craigmiller160.tolkienai.server.data.migration.mongo.MongoMigrationHelper
 
-class V001_InitialSchema : MongoCoreMigration {
-  override fun migrate(helper: MongoDatabase) {
-    helper.createCollection("chatLog")
-    val collection = helper.getCollection("chatLog")
+class V001_InitialSchema : MongoMigration {
+  override fun migrate(helper: MongoMigrationHelper) {
+    helper.database.createCollection("chatLog")
+    val collection = helper.database.getCollection("chatLog")
     collection.createIndex(Indexes.ascending("chat.chatId"))
     collection.createIndex(Indexes.ascending("timestamp"))
   }
