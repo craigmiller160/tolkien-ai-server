77b2726c2d2d197e5d32096db42709f35965be48
Craig Miller <craigmiller160@gmail.com>
2024-03-31 14:56:34 -0400

    Implemented test

--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationLoader.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationLoader.kt
@@ -3,13 +3,15 @@ package us.craigmiller160.tolkienai.server.data.migration
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory
 
-fun <T> loadMigrations(packagePaths: List<String>): List<Migration<T>> {
+fun <T> loadMigrations(vararg packagePaths: String): List<Migration<T>> {
   val resolver = PathMatchingResourcePatternResolver()
   val factory = CachingMetadataReaderFactory(resolver)
   return packagePaths
+      .asSequence()
       .flatMap { path -> resolver.getResources("$path/*.class").toList() }
       .map { resource -> factory.getMetadataReader(resource).classMetadata.className }
       .map { Class.forName(it).getDeclaredConstructor().newInstance() }
       .map { obj -> obj as Migration<T> }
       .sortedBy { it.javaClass.simpleName }
+      .toList()
 }
--- src/test/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationLoaderTest.kt
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationLoaderTest.kt
@@ -1,10 +1,29 @@
 package us.craigmiller160.tolkienai.server.data.migration
 
+import io.kotest.matchers.collections.shouldContainInOrder
+import io.kotest.matchers.collections.shouldHaveSize
 import org.junit.jupiter.api.Test
+import us.craigmiller160.tolkienai.server.data.migration.test_migrations.V20240330__InitialMigration
+import us.craigmiller160.tolkienai.server.data.migration.test_migrations.V20240331__MigrationTwo
+import us.craigmiller160.tolkienai.server.data.migration.test_migrations.V20240401__MigrationThree
+import us.craigmiller160.tolkienai.server.data.migration.test_migrations.bad.BadMockMigration
 
 class MigrationLoaderTest {
+  companion object {
+    private const val DEFAULT_MIGRATION_LOCATION =
+        "classpath:us/craigmiller160/tolkienai/server/data/migration/test_migrations"
+    private const val BAD_MIGRATION_LOCATION = "$DEFAULT_MIGRATION_LOCATION/bad"
+  }
+
   @Test
   fun `loads migrations`() {
-    TODO()
+    val migrations = loadMigrations<String>(DEFAULT_MIGRATION_LOCATION, BAD_MIGRATION_LOCATION)
+    migrations
+        .shouldHaveSize(4)
+        .shouldContainInOrder(
+            BadMockMigration(),
+            V20240330__InitialMigration(),
+            V20240331__MigrationTwo(),
+            V20240401__MigrationThree())
   }
 }
