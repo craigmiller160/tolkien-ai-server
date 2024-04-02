e19abf8a8aa83feca024733d71b82d0f7aef0f7d
Craig Miller <craigmiller160@gmail.com>
2024-04-02 19:40:31 -0400

    Stubbed out new API

--- src/main/kotlin/us/craigmiller160/tolkienai/server/web/controller/DataController.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/web/controller/DataController.kt
@@ -2,6 +2,7 @@ package us.craigmiller160.tolkienai.server.web.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,14 +10,20 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import us.craigmiller160.tolkienai.server.ai.ingestion.service.DataIngestionService
 import us.craigmiller160.tolkienai.server.web.type.IngestDataRequest
+import us.craigmiller160.tolkienai.server.web.type.RecordCountResponse
 
 @RestController
-@RequestMapping("/data-ingestion")
-class DataIngestionController(private val dataIngestionService: DataIngestionService) {
-  @PostMapping
+@RequestMapping("/data")
+class DataController(private val dataIngestionService: DataIngestionService) {
+  @PostMapping("/ingest")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   @PreAuthorize("hasAuthority('ROLE_admin')")
   fun ingestData(@RequestBody request: IngestDataRequest) {
     dataIngestionService.ingest(request.dryRun)
+  }
+
+  @GetMapping("/count")
+  fun getRecordCount(): RecordCountResponse {
+    TODO()
   }
 }
--- src/test/kotlin/us/craigmiller160/tolkienai/server/web/controller/DataControllerTest.kt
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/web/controller/DataControllerTest.kt
@@ -3,7 +3,7 @@ package us.craigmiller160.tolkienai.server.web.controller
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-class DataIngestionControllerTest {
+class DataControllerTest {
   @Test
   @Disabled
   fun `validate that admin access is required to ingest data`() {
