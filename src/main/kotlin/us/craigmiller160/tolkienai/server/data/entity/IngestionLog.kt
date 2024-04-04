f1e99b45055af991c4c65f5e4d2a440174679392
Craig Miller <craigmiller160@gmail.com>
2024-04-04 16:16:17 -0400

    Updated migration

--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/entity/IngestionLog.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/entity/IngestionLog.kt
@@ -4,8 +4,11 @@ import java.time.ZonedDateTime
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
+data class IngestionDetails(val characters: Int, val segments: Int, val tokens: Tokens)
+
 @Document(collection = "ingestionLog")
 data class IngestionLog(
+    val details: IngestionDetails,
     val timestamp: ZonedDateTime = ZonedDateTime.now(),
     @field:Id val id: String? = null
 )
---
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/entity/Tokens.kt
@@ -0,0 +1,3 @@
+package us.craigmiller160.tolkienai.server.data.entity
+
+data class Tokens(val prompt: Int, val chat: Int, val total: Int)
