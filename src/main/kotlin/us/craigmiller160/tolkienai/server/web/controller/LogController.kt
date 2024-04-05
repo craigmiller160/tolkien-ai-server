d1bc2d386259acc793d2901200542346c0b61eed
Craig Miller <craigmiller160@gmail.com>
2024-04-04 22:46:24 -0400

    Integrated DTOs into controller

--- src/main/kotlin/us/craigmiller160/tolkienai/server/web/controller/LogController.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/web/controller/LogController.kt
@@ -3,11 +3,18 @@ package us.craigmiller160.tolkienai.server.web.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import us.craigmiller160.tolkienai.server.web.type.ChatLogSearchRequest
+import us.craigmiller160.tolkienai.server.web.type.ChatLogSearchResponse
+import us.craigmiller160.tolkienai.server.web.type.IngestionLogSearchRequest
+import us.craigmiller160.tolkienai.server.web.type.IngestionLogSearchResponse
 
 @RestController
 @RequestMapping("/logs")
 class LogController {
-  @GetMapping("/ingestion") fun getIngestionLogs(): Unit = TODO()
+  @GetMapping("/ingestion")
+  fun searchForIngestionLogs(request: IngestionLogSearchRequest): IngestionLogSearchResponse =
+      TODO()
 
-  @GetMapping("/chat") fun getChatLogs(): Unit = TODO()
+  @GetMapping("/chat")
+  fun searchForChatLogs(request: ChatLogSearchRequest): ChatLogSearchResponse = TODO()
 }
