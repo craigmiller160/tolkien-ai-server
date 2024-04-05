424673c577f93b04bc741395b8ca72baa395cd30
Craig Miller <craigmiller160@gmail.com>
2024-04-04 22:40:16 -0400

    Stubbed controller endpoints

---
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/web/controller/LogControllerTest.kt
@@ -0,0 +1,18 @@
+package us.craigmiller160.tolkienai.server.web.controller
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class LogControllerTest {
+  @Test
+  @Disabled
+  fun `all options for getting chat logs`() {
+    TODO()
+  }
+
+  @Test
+  @Disabled
+  fun `all options for getting ingestion logs`() {
+    TODO()
+  }
+}
