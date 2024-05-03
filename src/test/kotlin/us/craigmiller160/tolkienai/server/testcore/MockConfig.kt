4b1ac9f7d61d2e5d8e4c3f84d7d9483e29796f70
Craig Miller <craigmiller160@gmail.com>
2024-05-03 19:07:49 -0400

    Mocking weaviate and openai

--- src/test/kotlin/us/craigmiller160/tolkienai/server/testcore/MockConfig.kt
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/testcore/MockConfig.kt
@@ -1,6 +1,7 @@
 package us.craigmiller160.tolkienai.server.testcore
 
 import com.aallam.openai.client.OpenAI
+import io.mockk.mockk
 import io.weaviate.client.WeaviateClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Profile
 @Profile("test")
 @Configuration
 class MockConfig {
-  @Bean fun weaviateClient(): WeaviateClient = TODO()
+  @Bean fun weaviateClient(): WeaviateClient = mockk<WeaviateClient>()
 
-  @Bean fun openaiClient(): OpenAI = TODO()
+  @Bean fun openaiClient(): OpenAI = mockk<OpenAI>()
 }
