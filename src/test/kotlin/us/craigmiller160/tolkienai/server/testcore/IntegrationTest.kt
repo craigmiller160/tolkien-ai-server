762372f5917ab3747f1dfb0533d3d5d5c7f04565
Craig Miller <craigmiller160@gmail.com>
2024-05-03 19:04:17 -0400

    Setting up integration test

---
+++ src/test/kotlin/us/craigmiller160/tolkienai/server/testcore/IntegrationTest.kt
@@ -0,0 +1,16 @@
+package us.craigmiller160.tolkienai.server.testcore
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import us.craigmiller160.testcontainers.common.spring.SpringTestContainersExtension
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@SpringBootTest
+@ExtendWith(value = [SpringTestContainersExtension::class, SpringExtension::class])
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+annotation class IntegrationTest
