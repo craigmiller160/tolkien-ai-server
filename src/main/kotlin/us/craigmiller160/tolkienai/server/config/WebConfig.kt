0af29d4fef1e913236b0a3a96580647ef1d2e176
Craig Miller <craigmiller160@gmail.com>
2024-05-04 14:10:35 -0400

    Implementing converters

---
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/WebConfig.kt
@@ -0,0 +1,31 @@
+package us.craigmiller160.tolkienai.server.config
+
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.Converter
+import org.springframework.data.convert.ReadingConverter
+import org.springframework.data.convert.WritingConverter
+import org.springframework.format.FormatterRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import us.craigmiller160.tolkienai.server.web.type.TIMESTAMP_FORMATTER
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+  override fun addFormatters(registry: FormatterRegistry) {
+    registry.addConverter(ZonedDateTimeToStringConverter())
+    registry.addConverter(StringToZonedDateTimeConverter())
+  }
+}
+
+@ReadingConverter
+class ZonedDateTimeToStringConverter : Converter<ZonedDateTime, String> {
+  override fun convert(dateTime: ZonedDateTime): String =
+      dateTime.withZoneSameInstant(ZoneId.of("UTC")).format(TIMESTAMP_FORMATTER)
+}
+
+@WritingConverter
+class StringToZonedDateTimeConverter : Converter<String, ZonedDateTime> {
+  override fun convert(source: String): ZonedDateTime =
+      ZonedDateTime.parse(source, TIMESTAMP_FORMATTER)
+}
--- src/main/kotlin/us/craigmiller160/tolkienai/server/web/type/IngestionLogSearchRequest.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/web/type/IngestionLogSearchRequest.kt
@@ -1,11 +1,10 @@
 package us.craigmiller160.tolkienai.server.web.type
 
 import java.time.ZonedDateTime
-import org.springframework.format.annotation.DateTimeFormat
 
 data class IngestionLogSearchRequest(
     override val pageNumber: Int,
     override val pageSize: Int,
-    @field:DateTimeFormat(pattern = TIMESTAMP_FORMAT) val startTimestamp: ZonedDateTime?,
-    @field:DateTimeFormat(pattern = TIMESTAMP_FORMAT) val endTimestamp: ZonedDateTime?
+    val startTimestamp: ZonedDateTime?,
+    val endTimestamp: ZonedDateTime?
 ) : PageRequest
