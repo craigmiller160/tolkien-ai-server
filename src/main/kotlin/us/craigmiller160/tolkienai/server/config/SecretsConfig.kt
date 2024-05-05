547f047e16bd9d15c4c4abbb4b3e2c1deab42d42
Craig Miller <craigmiller160@gmail.com>
2024-05-05 11:01:16 -0400

    Cleanup

--- src/main/kotlin/us/craigmiller160/tolkienai/server/config/SecretsConfig.kt
+++
@@ -1,8 +0,0 @@
-package us.craigmiller160.tolkienai.server.config
-
-// @Configuration
-// @PropertySource(
-//    ignoreResourceNotFound = true,
-//    value = ["file:./secrets.yml"],
-//    factory = YamlPropertySourceFactory::class)
-// class SecretsConfig
