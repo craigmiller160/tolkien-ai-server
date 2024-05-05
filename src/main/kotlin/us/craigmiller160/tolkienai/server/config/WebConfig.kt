71c0a009770779864fe02c0ce41c1664ffab77ec
Craig Miller <craigmiller160@gmail.com>
2024-05-04 22:52:22 -0400

    Working on serializers

--- src/main/kotlin/us/craigmiller160/tolkienai/server/config/WebConfig.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/WebConfig.kt
@@ -1,14 +1,10 @@
 package us.craigmiller160.tolkienai.server.config
 
-import java.time.ZoneId
-import java.time.ZonedDateTime
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.convert.converter.Converter
-import org.springframework.data.convert.ReadingConverter
-import org.springframework.data.convert.WritingConverter
 import org.springframework.format.FormatterRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import us.craigmiller160.tolkienai.server.web.type.TIMESTAMP_FORMATTER
+import us.craigmiller160.tolkienai.server.web.type.converters.StringToZonedDateTimeConverter
+import us.craigmiller160.tolkienai.server.web.type.converters.ZonedDateTimeToStringConverter
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
@@ -16,16 +12,4 @@ class WebConfig : WebMvcConfigurer {
     registry.addConverter(ZonedDateTimeToStringConverter())
     registry.addConverter(StringToZonedDateTimeConverter())
   }
-}
-
-@ReadingConverter
-class ZonedDateTimeToStringConverter : Converter<ZonedDateTime, String> {
-  override fun convert(dateTime: ZonedDateTime): String =
-      dateTime.withZoneSameInstant(ZoneId.of("UTC")).format(TIMESTAMP_FORMATTER)
-}
-
-@WritingConverter
-class StringToZonedDateTimeConverter : Converter<String, ZonedDateTime> {
-  override fun convert(source: String): ZonedDateTime =
-      ZonedDateTime.parse(source, TIMESTAMP_FORMATTER.withZone(ZoneId.of("UTC")))
 }
