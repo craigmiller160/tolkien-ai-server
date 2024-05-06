dd23123762d847dd9d2fa49201686c64cb1c69d8
Craig Miller <craigmiller160@gmail.com>
2024-05-06 16:06:10 -0400

    Trying to make weaviate support different dev & prod classes

--- src/main/kotlin/us/craigmiller160/tolkienai/server/config/WeaviateProperties.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/WeaviateProperties.kt
@@ -3,4 +3,9 @@ package us.craigmiller160.tolkienai.server.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "tolkienai.weaviate")
-data class WeaviateProperties(val host: String, val scheme: String, val key: String)
+data class WeaviateProperties(
+    val host: String,
+    val scheme: String,
+    val key: String,
+    val className: String
+)
