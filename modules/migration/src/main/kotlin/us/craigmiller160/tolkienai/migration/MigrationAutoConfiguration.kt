7da7feef29f394e64c12abf35a9b19eb93baccf8
Craig Miller <craigmiller160@gmail.com>
2024-05-06 16:40:58 -0400

    Progress

--- modules/migration/src/main/kotlin/us/craigmiller160/tolkienai/migration/MigrationAutoConfiguration.kt
+++ modules/migration/src/main/kotlin/us/craigmiller160/tolkienai/migration/MigrationAutoConfiguration.kt
@@ -1,8 +1,10 @@
 package us.craigmiller160.tolkienai.migration
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConfigurationPropertiesScan(basePackages = ["us.craigmiller160.tolkienai.migration"])
-class MigrationAutoConfiguration {}
+@ComponentScan(basePackages = ["us.craigmiller160.tolkienai.migration"])
+class MigrationAutoConfiguration
