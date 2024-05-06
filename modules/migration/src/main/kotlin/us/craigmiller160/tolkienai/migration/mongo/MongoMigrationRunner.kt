5f875104bb244673e432a117464e84b1a11ab81d
Craig Miller <craigmiller160@gmail.com>
2024-05-06 16:54:21 -0400

    Progress

--- modules/migration/src/main/kotlin/us/craigmiller160/tolkienai/migration/mongo/MongoMigrationRunner.kt
+++ modules/migration/src/main/kotlin/us/craigmiller160/tolkienai/migration/mongo/MongoMigrationRunner.kt
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Component
 import us.craigmiller160.tolkienai.migration.AbstractMigrationImplementationRunner
-import us.craigmiller160.tolkienai.server.config.MigrationProperties
+import us.craigmiller160.tolkienai.migration.config.MigrationProperties
 
 @Component
 class MongoMigrationRunner(
@@ -14,8 +14,8 @@ class MongoMigrationRunner(
     migrationProperties: MigrationProperties,
     @Value("\${spring.data.mongodb.database}") private val database: String
 ) :
-    us.craigmiller160.tolkienai.migration.AbstractMigrationImplementationRunner<
-        MongoMigrationHelper>(mongoTemplate, migrationProperties.mongo) {
+    AbstractMigrationImplementationRunner<MongoMigrationHelper>(
+        mongoTemplate, migrationProperties.mongo) {
   override val helper =
       MongoMigrationHelper(database = client.getDatabase(database), template = mongoTemplate)
 
