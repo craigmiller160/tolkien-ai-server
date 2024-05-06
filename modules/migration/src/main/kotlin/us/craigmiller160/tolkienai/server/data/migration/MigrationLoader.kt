9c2934186515324a9e147f3daefc42c560236297
Craig Miller <craigmiller160@gmail.com>
2024-05-06 16:28:48 -0400

    Moved code to another module

--- modules/migration/src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationLoader.kt
+++ modules/migration/src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/MigrationLoader.kt
@@ -3,7 +3,9 @@ package us.craigmiller160.tolkienai.server.data.migration
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory
 
-fun <T> loadMigrations(vararg packagePaths: String): List<Migration<T>> {
+fun <T> loadMigrations(
+    vararg packagePaths: String
+): List<us.craigmiller160.tolkienai.server.data.migration.Migration<T>> {
   val resolver = PathMatchingResourcePatternResolver()
   val factory = CachingMetadataReaderFactory(resolver)
   return packagePaths
@@ -11,9 +13,13 @@ fun <T> loadMigrations(vararg packagePaths: String): List<Migration<T>> {
       .flatMap { path -> resolver.getResources("$path/*.class").toList() }
       .map { resource -> factory.getMetadataReader(resource).classMetadata.className }
       .map { Class.forName(it) }
-      .filter { clazz -> Migration::class.java.isAssignableFrom(clazz) }
+      .filter { clazz ->
+        us.craigmiller160.tolkienai.server.data.migration.Migration::class
+            .java
+            .isAssignableFrom(clazz)
+      }
       .map { clazz -> clazz.getDeclaredConstructor().newInstance() }
-      .map { obj -> obj as Migration<T> }
+      .map { obj -> obj as us.craigmiller160.tolkienai.server.data.migration.Migration<T> }
       .sortedBy { it.javaClass.simpleName }
       .toList()
 }
