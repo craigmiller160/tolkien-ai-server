4d6b50123bd888fe5e410a767d7c7b3a70314052
Craig Miller <craigmiller160@gmail.com>
2024-05-06 16:35:14 -0400

    Progress with library setup

--- modules/migration/build.gradle.kts
+++ modules/migration/build.gradle.kts
@@ -5,14 +5,24 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 plugins {
     val kotlinVersion = "2.0.0-Beta5"
 
+    id("org.springframework.boot") version "3.2.4"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     id("io.craigmiller160.gradle.defaults") version "1.2.2"
     id("com.diffplug.spotless") version "6.17.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_20
+}
+
+dependencies {
+    compileOnly("org.slf4j:slf4j-api:2.0.13")
+    compileOnly("org.springframework.data:spring-data-mongodb")
+    compileOnly("io.weaviate:client:4.6.0") {
+        exclude("commons-logging", "commons-logging")
+    }
 }
 
 kotlin {
