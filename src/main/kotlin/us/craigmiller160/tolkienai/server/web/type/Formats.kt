05b9d4abe27fe61d024f5824c9df6bca42dda9e0
Craig Miller <craigmiller160@gmail.com>
2024-05-05 20:19:55 -0400

    Fixed warning

--- src/main/kotlin/us/craigmiller160/tolkienai/server/web/type/Formats.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/web/type/Formats.kt
@@ -3,4 +3,4 @@ package us.craigmiller160.tolkienai.server.web.type
 import java.time.format.DateTimeFormatter
 
 const val TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-val TIMESTAMP_FORMATTER = DateTimeFormatter.ofPattern(TIMESTAMP_FORMAT)
+val TIMESTAMP_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern(TIMESTAMP_FORMAT)
