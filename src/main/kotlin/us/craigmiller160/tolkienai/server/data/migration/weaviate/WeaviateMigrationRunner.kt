3f646d964e10d915eb49eccaaa33ab759ff54500
Craig Miller <craigmiller160@gmail.com>
2024-05-06 16:07:53 -0400

    Refactoring migration

--- src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/weaviate/WeaviateMigrationRunner.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/data/migration/weaviate/WeaviateMigrationRunner.kt
@@ -4,16 +4,19 @@ import io.weaviate.client.WeaviateClient
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Component
 import us.craigmiller160.tolkienai.server.config.MigrationProperties
+import us.craigmiller160.tolkienai.server.config.WeaviateProperties
 import us.craigmiller160.tolkienai.server.data.migration.AbstractMigrationImplementationRunner
 
 @Component
 class WeaviateMigrationRunner(
     weaviateClient: WeaviateClient,
     mongoTemplate: MongoTemplate,
-    migrationProperties: MigrationProperties
+    migrationProperties: MigrationProperties,
+    weaviateProperties: WeaviateProperties
 ) :
-    AbstractMigrationImplementationRunner<WeaviateClient>(
+    AbstractMigrationImplementationRunner<WeaviateMigrationHelper>(
         mongoTemplate, migrationProperties.weaviate) {
   override val collectionName: String = "weaviate_migration_history"
-  override val helper: WeaviateClient = weaviateClient
+  override val helper: WeaviateMigrationHelper =
+      WeaviateMigrationHelper(client = weaviateClient, className = weaviateProperties.className)
 }
