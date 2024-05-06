8f10a9fe6a8c030729cfccc3ff0439437831acef
Craig Miller <craigmiller160@gmail.com>
2024-05-05 20:17:29 -0400

    Progress

--- src/main/kotlin/us/craigmiller160/tolkienai/server/config/OpenApiConfig.kt
+++ src/main/kotlin/us/craigmiller160/tolkienai/server/config/OpenApiConfig.kt
@@ -3,6 +3,7 @@ package us.craigmiller160.tolkienai.server.config
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.SpecVersion
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -17,5 +18,12 @@ class OpenApiConfig {
               version = "v1"
               specVersion = SpecVersion.V31
             }
+
+        servers =
+            listOf(
+                Server().apply {
+                  url = "http://localhost:8080"
+                  description = "API Server URL"
+                })
       }
 }
